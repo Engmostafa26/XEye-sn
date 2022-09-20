1cca17c03c7e2bde35e02765d20d04a72db8db3c
Mostafa Ahmad <53053339+Engmostafa26@users.noreply.github.com>
2022-09-20 14:42:07 +0200

    Update XEye-sn.py

--- XEye-sn.py
+++ XEye-sn.py
@@ -33,9 +33,9 @@ def paket(paket):
             keyws = ["uname", "username", "Username", "Email", "Emails", "email", "emails", "pass", "pasw", "id", "userid"]
             for keyw in keyws:
                 if keyw in str(cred):
-                    print("\n*****Congrats*****Congrats*****Congrats*****Congrats*****Congrats*****Congrats")
+                    print("\n-------Congrats-------Congrats-------Congrats-------Congrats-------Congrats-------Congrats")
                     print(" [Congrats] --> Credintials detected in this line: --> "+str(cred))
-                    print("*****Congrats*****Congrats*****Congrats*****Congrats*****Congrats*****Congrats\n")
+                    print("\n-------Congrats-------Congrats-------Congrats-------Congrats-------Congrats-------Congrats")
                     break
 def Checkroot():
     who = subprocess.check_output('whoami')
