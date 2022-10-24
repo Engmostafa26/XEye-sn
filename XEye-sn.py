4dacaa3aea558e6a251c4bb8f9d8e17ad8162488
Mostafa Ahmad <53053339+Engmostafa26@users.noreply.github.com>
2022-10-24 18:32:42 +0200

    Update XEye-sn.py

--- XEye-sn.py
+++ XEye-sn.py
@@ -76,3 +76,7 @@ def udte():
             print(" [Assistance] --> If you need any further assistance, please contact us on our Facebook page: https://facebook.com/XEyecs")
             exit()
 Checkroot()
+print("\t\t\t\t[*] Thanks for using XEye-tp. Below are our Social Media OSINT Hacking bundle recommended for you :) [*]")
+print("\n [***] --> The Ultimate Social Media OSINT Hacking Bundle(70% OFF): https://rb.gy/sgxib8")
+print("*******************************************************************************************************")
+print("\n [Author] Eng.Mostafa Ahmad - Cybersecurity Expert and \"XEye\" founder.")
